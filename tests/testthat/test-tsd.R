79652a07553314dd387c236273cd76b56b9c0215
Kasper Schou Telkamp <kasper.telkamp@gmail.com>
2023-10-26 15:39:49 +0200

    Linting failure. Fixes #1

--- tests/testthat/test-tsd.R
+++ tests/testthat/test-tsd.R
@@ -2,7 +2,8 @@ test_that("Can correctly make an 'aedseo_tsd' class object", {
   tsd_day <- tsd(
     observed = c(10, 15, 20, 18),
     time = as.Date(c("2023-01-01", "2023-01-02", "2023-01-03", "2023-01-05")),
-    time_interval = "day")
+    time_interval = "day"
+  )
 
   expect_s3_class(object = tsd_day, class = "aedseo_tsd")
 })
