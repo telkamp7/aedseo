0f61d85cb4eb1a8c3d7a38b6c80441331f912e62
Kasper Schou Telkamp <kasper.telkamp@gmail.com>
2023-11-02 09:54:57 +0100

    Added `@importsFrom ggplot2 autoplot`

--- R/autoplot.R
+++ R/autoplot.R
@@ -1,6 +1,5 @@
 #' Create a complete ggplot appropriate to a particular data type
 #'
-#' @rdname autoplot
 #'
 #' @description
 #' `r lifecycle::badge("experimental")`
@@ -11,12 +10,13 @@
 #'
 #' @param object An `aedseo_tsd` or `aedseo` object
 #' @param linewidth Numeric, the width of the line for the growth rate
-#' @param alpha Numeric, the alpha (transparency) for the confidence interval ribbon
+#' @param alpha Numeric, the alpha (transparency) for the confidence interval
+#' ribbon
 #' @param ... Additional arguments (not used).
 #'
 #' @return A ggplot object for visualizing the time series data.
 #'
-#' @export
+#' @aliases autoplot
 #'
 #' @examples
 #' # Create an example aedseo_tsd object
@@ -34,28 +34,40 @@
 #' )
 #'
 #' # Create a ggplot visualization for the aedseo_tsd object
-#' #autoplot(aedseo_tsd_object)
+#' # autoplot(aedseo_tsd_object)
 #'
 #' # Create an aedseo object
 #' aedseo_object <- aedseo(
 #'   tsd = aedseo_tsd_object,
 #'   k = 3,
 #'   level = 0.95,
-#'   family = "quasipoisson")
+#'   family = "quasipoisson"
+#' )
 #'
 #' # Create a ggplot visualization of growth rates with confidence intervals
-#' #autoplot(aedseo_object, linewidth = 1, alpha = 0.2)
+#' # autoplot(aedseo_object, linewidth = 1, alpha = 0.2)
+#' @importFrom ggplot2 autoplot
+#' @export
+autoplot <- function(object, ...) {
+  UseMethod("autoplot")
+}
+#' @rdname autoplot
+#' @method autoplot aedseo_tsd
+#' @export
 autoplot.aedseo_tsd <- function(object, ...) {
   object %>%
     ggplot2::ggplot(
       mapping = ggplot2::aes(
         x = .data$time,
-        y = .data$observed)) +
+        y = .data$observed
+      )
+    ) +
     ggplot2::geom_point() +
     ggplot2::geom_line()
 }
 
 #' @rdname autoplot
+#' @method autoplot aedseo
 #' @export
 autoplot.aedseo <- function(object, linewidth = 0.7, alpha = 0.3, ...) {
   object %>%
@@ -64,7 +76,9 @@ autoplot.aedseo <- function(object, linewidth = 0.7, alpha = 0.3, ...) {
         x = .data$reference_time,
         y = .data$growth_rate,
         ymin = .data$lower_growth_rate,
-        ymax = .data$upper_growth_rate)) +
+        ymax = .data$upper_growth_rate
+      )
+    ) +
     ggplot2::geom_line(linewidth = linewidth) +
     ggplot2::geom_ribbon(alpha = alpha)
 }
