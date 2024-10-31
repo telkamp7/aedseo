eb75bf967ceaa87f7554cfa7f69a1f4301d856bf
SofiaOtero <myrup.otero@gmail.com>
2024-10-31 14:23:14 +0100

    Minor fixes

--- tests/testthat/test-intensity_levels.R
+++ tests/testthat/test-intensity_levels.R
@@ -17,23 +17,39 @@ test_that("Test that input argument checks work", {
 
   expect_no_error(intensity_levels(tsd_data))
 
-  expect_error(intensity_levels(tsd_data, decay_factor = 3), "Variable 'decay_factor': Element 1 is not <= 1.")
+  expect_error(
+    checkmate_err_msg(intensity_levels(tsd_data, decay_factor = 3),
+      "Variable 'decay_factor': Element 1 is not <= 1."
+    )
+  )
 
-  expect_error(intensity_levels(tsd_data, n_peak = c(1, 2)), "Variable 'n_peak': Must have length 1, but has length 2.")
+  expect_error(
+    checkmate_err_msg(intensity_levels(tsd_data, n_peak = c(1, 2)),
+      "Variable 'n_peak': Must have length 1, but has length 2."
+    )
+  )
 
-  expect_error(checkmate_err_msg(intensity_levels(tsd_data, disease_threshold = "hey"),
-               "'disease_threshold': Must be of type 'integerish', not 'character'.",
-               fixed = TRUE))
+  expect_error(
+    checkmate_err_msg(intensity_levels(tsd_data, disease_threshold = "hey"),
+      "'disease_threshold': Must be of type 'integerish', not 'character'.",
+      fixed = TRUE
+    )
+  )
 
-  expect_error(intensity_levels(tsd_data, season_weeks = c(2, 10)), "`start` must be greater than `end`!")
+  expect_error(
+    checkmate_err_msg(intensity_levels(tsd_data, season_weeks = c(2, 10)),
+      "`start` must be greater than `end`!"
+    )
+  )
 
   tsd_fail <- tsd_data |> dplyr::rename(week = time)
 
   expect_error(
     checkmate_err_msg(intensity_levels(tsd_fail),
-    "Variable 'colnames(tsd)': Names must be a identical to set {'time','observed'}, but is {'week','observed'}.",
-    fixed = TRUE
-  ))
+      "Variable 'colnames(tsd)': Names must be a identical to set {'time','observed'}, but is {'week','observed'}.",
+      fixed = TRUE
+    )
+  )
 
   #Finally check that dots arguments work
   expect_no_error(intensity_levels(tsd_data, conf_levels = c(0.2, 0.5, 0.9)))
