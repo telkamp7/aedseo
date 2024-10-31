a51d65753735d3fb896cff7a470ac274207afade
SofiaOtero <myrup.otero@gmail.com>
2024-10-31 13:46:30 +0100

    Add rewiev suggestions

--- R/intensity_levels.R
+++ R/intensity_levels.R
@@ -3,26 +3,26 @@
 #' @description
 #' `r lifecycle::badge("stable")`
 #'
-#' This function calculates the intensity levels of time series observations that are stratified by season.
+#' This function calculates the intensity levels of time series of observations that are stratified by season.
 #' It uses the previous seasons to calculate the intensity levels of the newest season.
 #' It can provide two different types of intensity levels, by using one of following functions:
-#' - `compute_weighted_intensity_levels` which calculates the lower, medium and high intensity levels.
-#' - `compute_relative_dist_intensity_levels` which calculates the very low, low, medium and high
-#'    intensity levels with use of the highest intensity level from `compute_weighted_intensity_levels`
+#' - `compute_weighted_intensity_levels()` which calculates the "low", "medium" and "high" intensity levels.
+#' - `compute_relative_dist_intensity_levels()` which calculates the "very low", "low", "medium" and "high"
+#'    intensity levels with use of the highest intensity level from `compute_weighted_intensity_levels()`
 #'    as the high intensity level and the disease_threshold as the very low intensity level.
 #'
 #' @param tsd A `aedseo_tsd` object containing time series data with 'time' and 'observed'.
 #' @param decay_factor A numeric value between 0 and 1, that specifies the weight applied to previous seasons in
-#' calculations. It is used as `decay_factor`^(seasons back), whereby the weight for the most recent season will be
-#' 0.8^0 = 1. This parameter allows for a decreasing weight assigned to prior seasons, such that as the number of
-#' previous seasons increases, the influence of older seasons diminishes exponentially.
+#' calculations. It is used as `decay_factor`^(number of seasons back), whereby the weight for the most recent season
+#' will be `decay_factor`^0 = 1. This parameter allows for a decreasing weight assigned to prior seasons, such that
+#' the influence of older seasons diminishes exponentially.
 #' @param disease_threshold An integer specifying the threshold for considering a disease outbreak. It defines the per
 #' time-step disease threshold that has to be surpassed for the observation to be included in the calculations.
 #' @param n_peak A numeric value specifying the number of peak observations to be selected from each season in the
-#' intensity level calculations. The n_peak observations have to surpass the disease_threshold to be included.
-#' @param season_weeks A numeric vector of length 2, `c(start,end)`, with the start and end weeks of the seasons to
-#' stratify the observations by. Must span the new year; ex: `season_weeks = c(21, 20)`.
-#' @param ... arguments that can be passed to the `compute_weighted_intensity_levels` function.
+#' intensity level calculations. The `n_peak` observations have to surpass the `disease_threshold` to be included.
+#' @param season_weeks A numeric vector of length 2, `c(start, end)`, with the start and end weeks of the seasons to
+#' stratify the observations by. Must span the new year; e.g.: `season_weeks = c(21, 20)`.
+#' @param ... arguments that can be passed to the `compute_weighted_intensity_levels()` function.
 
 intensity_levels <- function(
   tsd,
@@ -51,18 +51,16 @@ intensity_levels <- function(
 
   # Add weights and remove newest season to get predictions for this season
   weighted_seasonal_tsd <- seasonal_tsd |>
-    dplyr::filter(!.data$season == max(.data$season)) |>
-    dplyr::mutate(year = purrr::map_chr(.data$season, ~ strsplit(.x, "/")[[1]][2]) |>
+    dplyr::filter(.data$season != max(.data$season)) |>
+    dplyr::mutate(year = purrr::map_chr(.data$season, ~ stringr::str_extract(.x, "[0-9]+")) |>
                     as.numeric()) |>
     dplyr::mutate(weight = decay_factor^(max(.data$year) - .data$year))
 
   # Select n_peak highest observations and filter observations >= disease_threshold
   season_observations_and_weights <- weighted_seasonal_tsd |>
     dplyr::select(-c("year", "time")) |>
-    dplyr::group_by(.data$season) |>
-    dplyr::slice_max(.data$observed, n = n_peak, with_ties = FALSE) |>
     dplyr::filter(.data$observed >= disease_threshold) |>
-    dplyr::ungroup()
+    dplyr::slice_max(.data$observed, n = n_peak, with_ties = FALSE, by = "season")
 
   # Run `compute_weighted_intensity_levels` on data to get peak intensity levels
   computed_peak_levels <- compute_weighted_intensity_levels(weighted_observations = season_observations_and_weights |>
@@ -70,8 +68,8 @@ intensity_levels <- function(
 
   # Select the newest season for intensity level output
   season_peak_levels <- computed_peak_levels |>
-    dplyr::mutate(season = unique(max(seasonal_tsd$season))) |>
-    dplyr::relocate(.data$season, .before = dplyr::everything())
+    dplyr::mutate(season = max(seasonal_tsd$season)) |>
+    dplyr::select(.data$season, dplyr::everything())
 
   return(season_peak_levels)
 }
--- tests/testthat/test-intensity_levels.R
+++ tests/testthat/test-intensity_levels.R
@@ -7,7 +7,7 @@ test_that("Test that input argument checks work", {
                            to = end_date,
                            by = "week")
 
-  obs <- rpois(length(weekly_dates), 1000)
+  obs <- stats::rpois(length(weekly_dates), 1000)
 
   tsd_data <- tsd(
     observed = obs,
@@ -17,17 +17,23 @@ test_that("Test that input argument checks work", {
 
   expect_no_error(intensity_levels(tsd_data))
 
-  expect_error(intensity_levels(tsd_data, decay_factor = 3))
+  expect_error(intensity_levels(tsd_data, decay_factor = 3), "Variable 'decay_factor': Element 1 is not <= 1.")
 
-  expect_error(intensity_levels(tsd_data, n_peak = c(1, 2)))
+  expect_error(intensity_levels(tsd_data, n_peak = c(1, 2)), "Variable 'n_peak': Must have length 1, but has length 2.")
 
-  expect_error(intensity_levels(tsd_data, disease_threshold = "hey"))
+  expect_error(checkmate_err_msg(intensity_levels(tsd_data, disease_threshold = "hey"),
+               "'disease_threshold': Must be of type 'integerish', not 'character'.",
+               fixed = TRUE))
 
-  expect_error(intensity_levels(tsd_data, season_weeks = c(2, 10)))
+  expect_error(intensity_levels(tsd_data, season_weeks = c(2, 10)), "`start` must be greater than `end`!")
 
   tsd_fail <- tsd_data |> dplyr::rename(week = time)
 
-  expect_error(intensity_levels(tsd_fail))
+  expect_error(
+    checkmate_err_msg(intensity_levels(tsd_fail),
+    "Variable 'colnames(tsd)': Names must be a identical to set {'time','observed'}, but is {'week','observed'}.",
+    fixed = TRUE
+  ))
 
   #Finally check that dots arguments work
   expect_no_error(intensity_levels(tsd_data, conf_levels = c(0.2, 0.5, 0.9)))
@@ -37,7 +43,7 @@ test_that("Test that input argument checks work", {
 
 })
 
-test_that("Test we get correct season output for newest season", {
+test_that("Test that we get correct season output for newest season", {
 
   start_date <- as.Date("2021-01-04")
   end_date <- as.Date("2023-12-31")
@@ -57,7 +63,7 @@ test_that("Test we get correct season output for newest season", {
   seasonal_tsd <- tsd_data |>
     dplyr::mutate(season = epi_calendar(.data$time, start = 21, end = 20))
 
-  newest_season <- unique(max(seasonal_tsd$season))
+  newest_season <- max(seasonal_tsd$season)
 
   intensity_levels <- intensity_levels(tsd_data)
 
